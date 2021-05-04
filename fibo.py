9409f92352e49d4dcd0fef2d47f1c19e726d7738
Ondřej Bekr <bekrinda@gmail.com>
2021-05-04 17:14:44 +0200

    Vypocet n-tého prvku Fibonacciho posloupnosti

--- fibo.py
+++ fibo.py
@@ -1,6 +1,22 @@
 
+def recursive_nth_fibo(n):
+    """
+    Fce vrátí Fibonacciho posloupnosti pomocí D&C
+    :param n: (int) n-tý prvek, který chceme zjistit
+    :return: (int) n-tý prvek posloupnosti
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
+
 def main():
-    pass
+    n = int(input("Zadejte počet prvků posloupnosti: "))
+    posl = [recursive_nth_fibo(i) for i in range(n)]
+    print(posl)
 
 
 if __name__ == '__main__':
